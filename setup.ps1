855513183c0008af839c393e5bb857772abc1b9f
Dr. Felix Heinrichs <fheinrichs@heinrichs.it>
2020-02-11 22:34:18 +0100

    added alacritty configuration

--- setup.ps1
+++ setup.ps1
@@ -1,3 +1,20 @@
+Write-Output "preparing environment"
+
+
+# setup alacritty
+Write-Output ".. configuring alacritty"
+
+$alacritty_config = "$home\AppData\Roaming\alacritty"
+
+if (!(Test-Path -Path $nvim_config -PathType Container)) {
+    New-Item -Path $alacritty_config -ItemType Directory -Force *> $null
+}
+if (Test-Path -Path "$alacritty_config\alacritty.yml" -PathType Leaf) {
+    Rename-Item -Path "$alacritty_config\alacritty.yml" -NewName "alacritty.yml.bak" *> $null
+} 
+New-Item -ItemType SymbolicLink -Path "$alacritty_config\alacritty.yml" -Target "$(Get-Location)\alacritty\alacritty.yml" *> $null
+
+
 # setup neovim
 Write-Output ".. configuring neovim"
 
@@ -15,7 +32,6 @@ if (Test-Path -Path "$nvim_config\init.vim" -PathType Leaf) {
 New-Item -ItemType SymbolicLink -Path "$nvim_config\init.vim" -Target "$(Get-Location)\nvim\init.vim" *> $null
 
 # install/update vim-plug
-
 if (!(Test-Path -Path $nvim_autoload -PathType Container)) {
     New-Item -Path $nvim_autoload -ItemType Directory -Force *> $null
 }
@@ -30,5 +46,5 @@ if (!(Test-Path -Path "$nvim_autoload\plug.vim" -PathType Leaf)) {
 }
 
 # execute installation in nvim
-Write-Output ".. installing plugins"
+Write-Output ".. installing neovim plugins"
 nvim --headless +PlugInstall +qa *> $null
