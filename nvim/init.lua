d84fbaf9cccb7cbd1c78aed4a6cefb261129c17c
Dr. Felix Heinrichs <felix.heinrichs@arabesque.com>
2022-11-20 19:00:14 +0100

    feat(nvim): lua based configuration

---
+++ nvim/init.lua
@@ -0,0 +1,19 @@
+-- TODO setup packer for plugin management
+-- TODO basic config
+--
+--  helpers:  https://www.youtube.com/watch?v=ppMX4LHIuy4&t=475s (overview)
+--            https://www.youtube.com/watch?v=ctH-a-1eUME&list=PLhoH5vyxr6Qq41NFL4GvhFp-WLd5xzIzZ&index=1 (course)
+--            https://github.com/nanotee/nvim-lua-guide
+--
+-- TODO lsp-config https://github.com/neovim/nvim-lspconfig/wiki/Autocompletion
+-- TODO treesitter https://github.com/nvim-treesitter/nvim-treesitter
+-- TODO treesitter text objects https://github.com/nvim-treesitter/nvim-treesitter-textobjects
+-- TODO AsyncRun https://github.com/skywind3000/asyncrun.vim
+-- TODO Toggleterm https://github.com/akinsho/toggleterm.nvim (works with
+-- Asyncrun)
+-- TODO nvim surround https://github.com/kylechui/nvim-surround
+-- TODO nvim autopairs https://github.com/windwp/nvim-autopairs
+-- TODO comment plugin w/ treesitter support https://github.com/numToStr/Comment.nvim
+-- TODO fzf-lua https://github.com/ibhagwan/fzf-lua
+-- TODO mason for lsp management https://github.com/williamboman/mason.nvim
+-- TODO lua line https://github.com/nvim-lualine/lualine.nvim
