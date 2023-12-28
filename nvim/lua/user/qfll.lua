1e09ee4044114a6148b959dab7f790ea8e1b43cf
Dr. Felix Heinrichs <felix.heinrichs@esgbook.com>
2023-12-28 18:32:32 +0100

    feat(nvim): quickfix history navigation

--- nvim/lua/user/qfll.lua
+++ nvim/lua/user/qfll.lua
@@ -1,23 +1,55 @@
 local opts = { noremap = true, silent = true }
 local keymap = vim.api.nvim_set_keymap
 
--- quickfix / locationlist navigation
-keymap("n", "[l", ":lprevious<CR>", opts)
-keymap("n", "]l", ":lnext<CR>", opts)
+
+-- quickfix
+
 keymap("n", "[q", ":cprevious<CR>", opts)
 keymap("n", "]q", ":cnext<CR>", opts)
 
--- toggle quickfix
-vim.keymap.set("n", "<leader>q", function()
+local toggle_qf = function()
   local qfwin_id = vim.fn.getqflist({ winid = true }).winid
   if qfwin_id == 0 then
     vim.cmd("copen")
   else
     vim.cmd("cclose")
   end
+end
+
+vim.keymap.set("n", "<leader>qq", toggle_qf, opts)
+vim.keymap.set("n", "<leader>q", toggle_qf, opts)
+
+vim.keymap.set("n", "<leader>qh", function()
+  local ok, _ = pcall(function() vim.cmd("colder") end)
+  if not ok then
+    vim.print("[ at oldest quickfix ]")
+  end
 end, opts)
 
--- toggle location list
+vim.keymap.set("n", "<leader>ql", function()
+  local ok, _ = pcall(function() vim.cmd("cnewer") end)
+  if not ok then
+    vim.print("[ at newest quickfix ]")
+  end
+end, opts)
+
+-- autocommand to attach to qf list
+-- vim.api.nvim_create_autocmd({ "FileType" }, {
+--   pattern = { "qf" },
+--   group = vim.api.nvim_create_augroup("qf_file_config", { clear = true }),
+--   callback = function(_)
+--     local buf_opts = { noremap = true, silent = true, buffer = vim.api.nvim_buf_get_number(0) }
+--
+--     -- navigate through older / newer quickfixes
+--   end
+-- })
+
+
+-- location list
+
+keymap("n", "[l", ":lprevious<CR>", opts)
+keymap("n", "]l", ":lnext<CR>", opts)
+
 vim.keymap.set("n", "<leader>l", function()
   local ok, ll = pcall(vim.fn.getloclist, vim.api.nvim_win_get_number(0), { winid = true })
   if not ok then
@@ -25,8 +57,8 @@ vim.keymap.set("n", "<leader>l", function()
   end
 
   if ll.winid == 0 then
-    vim.cmd("lopen")
+    pcall(function() vim.cmd("lopen") end)
   else
-    vim.cmd("lclose")
+    pcall(function() vim.cmd("lclose") end)
   end
 end, opts)
